20956a5804423a13af4607c566b23839df0568de
Daniel Carrasco Marín <danixu86@outook.com>
2019-11-06 01:11:16 +0100

    Reverted to WX resizer (lower exe size and now transparency is fixed)

--- plugins/database/common/image.py
+++ plugins/database/common/image.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from modules import imageResize, compressionTools
+from modules import imageResizeWX, compressionTools
 from wx import BITMAP_TYPE_PNG, BITMAP_TYPE_JPEG, BITMAP_TYPE_BMP
 
 
@@ -21,7 +21,7 @@ def image_add(self, image, size, parent, category, format=BITMAP_TYPE_PNG,
         if format == BITMAP_TYPE_JPEG or format == BITMAP_TYPE_BMP:
             self.log.debug("Image has not transparent support")
             color = (255, 255, 255)
-        image = imageResize.imageResize(
+        image = imageResizeWX.imageResizeWX(
             image,
             nWidth=size[0],
             nHeight=size[1],
